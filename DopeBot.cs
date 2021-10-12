c9f72b6b8beee1253d9c45d346ffaecc039a28fa
wafflehammer <neitys@gmail.com>
2021-10-11 23:47:50 -0400

    bij;l

--- DopeBot.cs
+++ DopeBot.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using Discord.Net;
@@ -7,38 +11,90 @@ using DSharpPlus.CommandsNext;
 class DopeBot
     {
         static DiscordClient discord;
-
+        static string fileUrl = "https://gist.githubusercontent.com/wafflehammer/e40c46c7d0ebe1059e1fc00d3c2cd129/raw/2c9fd11103c8c939583cf6197b69a022f7858d9d/gistfile1.txt";
+        private static string tokenFileName = "token.txt";
+        static string fileNameToSave = "CrackMeSolutions.txt";
+        static string fullPathWhereToSave;
+        static int timeoutInMilliSec;
         static void Main(string[] args)
         {
+         
+            
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+
+        static async Task<string> DownloadDocument()
+        {  
+            fullPathWhereToSave = Path.Combine(  Path.GetTempPath(), fileNameToSave);
+            bool success = FileDownloader.DownloadFile(fileUrl, fullPathWhereToSave, timeoutInMilliSec);
+       
+            await Task.Delay(-1);
+            return System.IO.File.ReadAllText(fullPathWhereToSave);
+
+        }
+
+      
+        static async Task<string> ReadToken()
+        {
+            return System.IO.File.ReadAllText(Path.Combine(System.Environment.CurrentDirectory, tokenFileName));
+        }
+        struct solutiontable
+        {
+            private byte id;
+            private string solution;
+
+            public solutiontable(byte id, string solution)
+            {
+                this.solution = solution;
+                this.id = id;
+            }
         }
 
         static async Task MainAsync(string[] args)
         {
-            discord = new DiscordClient(new DiscordConfiguration
+
+            string readtoken = await ReadToken();
+
+            DiscordConfiguration config = new DiscordConfiguration
             {
-                Token = "NzAxMzAwODY0NTI2MzE5NjQ2.XpvfUw.Ho8Ma_aSmXq_qZ_XxSuN7VmSsGI",
-                TokenType = TokenType.Bot
-            });
+                TokenType = TokenType.Bot,
+                Token = readtoken
+            };
+     
+            discord = new DiscordClient(config);
+            
+            string downloadedtable = await DownloadDocument();
+           
+            
+            List <solutiontable> MasterTable = null;
+            string[] parsedtable = downloadedtable.Split("-", StringSplitOptions.RemoveEmptyEntries);
+            for (byte b = 0; b < parsedtable.Length; b += 2)
+            {
+                solutiontable entry = new solutiontable(byte.Parse(parsedtable[b]), parsedtable[b + 1]);
+                MasterTable.Add(entry);
+
+            }
+
+       
 
          discord.MessageCreated += async (s, e) =>
                  {
-                     if (e.Message.Content.ToLower().StartsWith("solve"))
+                     if (e.Message.Content.ToLower().StartsWith("..solve"))
                      {  //format is solve-0001-
-                         string solution;
                          try
                          {
                              string[] query = e.Message.Content.Split('-');
-                             int crackmeID = int.Parse(query[1]);
+                             int crackID = int.Parse(query[1]);
 
-                             solution = query[2];
-                             await e.Message.RespondAsync(crackmeID + " " + solution);
+                             var solution = query[2];
+                             await e.Message.RespondAsync(crackID + " " + solution);
                              await e.Message.DeleteAsync();
                          }
                          catch (Exception ex)
                          {
-                             await e.Message.RespondAsync("sql injecting the bot is not a valid crackme solution (actually, it totally is) so here is how and why you broke me: "); 
-                             await e.Message.RespondAsync(ex.ToString());
+                             await e.Message.RespondAsync("nigga wat?"); 
+                             //await e.Message.RespondAsync(ex.ToString());
                          }
 
                          
@@ -48,6 +104,9 @@ class DopeBot
          
             await discord.ConnectAsync();
             await Task.Delay(-1);
+            
+            
+            
         }
         }
         
